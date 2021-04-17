fe2285a55abf7f37f48eb146e7b8a980084fd4a7
Maciej Dobrowolski <45076427+Mahox99@users.noreply.github.com>
2021-04-18 00:35:25 +0200

    Load && save scores
    Versions for PC and Adroid platform.

---
+++ ScoreLoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class ScoreLoad : MonoBehaviour
+{
+    public string filename = "highscore.data";
+    public string Score, line;
+    public GameObject HighScoreDisplay;
+    public static int CompareScore, xxx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PC:
+        //StreamReader sr = new StreamReader(filename);
+        //line = sr.ReadLine();
+        //while (line != null)
+        //{
+        //    Score = line;
+        //    line = sr.ReadLine();
+        //}
+        //sr.Close();
+
+
+        //Android:
+
+        xxx = PlayerPrefs.GetInt("HighScore");   
+        HighScoreDisplay.GetComponent<UnityEngine.UI.Text>().text = "Best result: " + xxx;
+
+        //CompareScore = int.Parse(Score);
+    }
+
+ 
+}
---
+++ ScoreSave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScoreSave : MonoBehaviour
+{
+    public int Score;
+    public int HighScore;
+    public string filename = "highscore.data";
+
+    public void Start()
+    {
+        HighScore = ScoreLoad.CompareScore;
+        Score = Vechiclesystem.TheScore;
+        if (Score >= HighScore)
+        {
+            //PC:
+
+            //var OurFile = File.CreateText(filename);
+            //OurFile.WriteLine("" + Score);
+            //OurFile.Close();
+
+
+            //Android:            
+            PlayerPrefs.SetInt("HighScore", Score);
+        }
+    }
+}
