0cfaf64779bd3a990eaf1adf1b20598bde2f4148
Maciej Dobrowolski <45076427+Mahox99@users.noreply.github.com>
2021-04-17 12:28:26 +0200

    Player system
    Vechiclesystem tells to how many HP and SCORE do you have.
    Death script starts after death.

---
+++ Vechiclesystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vechiclesystem : MonoBehaviour
+{
+    public static int TheScore;
+    public static int HP = 100;
+    public GameObject ScoreDisplay;
+    public GameObject HPDisplay;
+
+    void Start()
+    {
+        HP = 100;
+        TheScore = 0;
+        InvokeRepeating("AddScore", 1, 0.5f);
+    }
+
+    void AddScore()
+    {
+        TheScore += 5;
+        ScoreDisplay.GetComponent<UnityEngine.UI.Text>().text = "Score : " + TheScore;
+        
+    }
+    void Update()
+    {
+        HPDisplay.GetComponent<UnityEngine.UI.Text>().text = "Hull : " + HP;
+    }
+
+}
