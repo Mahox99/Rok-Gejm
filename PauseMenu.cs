2cedac6814b598a87d0cb2b51c57b7ba16853f95
Maciej Dobrowolski <45076427+Mahox99@users.noreply.github.com>
2021-04-20 20:53:32 +0200

    Pause Menu

---
+++ PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+    public GameObject joistick;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+        joistick.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+        joistick.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void BackMenu()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+        NextAxis.TheXAxis = -134.1f;
+    }
+
+}
